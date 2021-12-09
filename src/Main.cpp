9312bea7ba0c6fb7b0379329a614d8625cc30e5f
Andrew King <pepsiotaku@gmail.com>
2021-12-08 22:06:43 -0500

    draw with fancy new SDL_RenderGeometry

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,61 +12,86 @@ int main(const int argc, const char* const argv[])
 
 	std::cout.flush();
 
-	// The window we'll be rendering to
 	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
 
-	// The surface contained by the window
-	SDL_Surface* screenSurface = nullptr;
-
-	// Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << "\n";
+		return 1;
 	}
-	else
+
+	constexpr int screenWidth = 640;
+	constexpr int screenHeight = 480;
+
+	bool quit = false;
+	window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
 	{
-		constexpr int screenWidth = 640;
-		constexpr int screenHeight = 480;
+		std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << "\n";
+		SDL_Quit();
+		return 1;
+	}
 
-		// Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
-		if (window == nullptr)
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == nullptr)
+	{
+		std::cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << "\n";
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+		return 1;
+	}
+
+	constexpr int vertSize = 4;
+	SDL_Vertex vert[vertSize];
+	int indices[6];
+
+	vert[0].position.x = 32;
+	vert[0].position.y = 32;
+	vert[0].color = { 255, 0, 0, 255 };
+
+	vert[1].position.x = 608;
+	vert[1].position.y = 32;
+	vert[1].color = { 0, 0, 0, 255 };
+
+	vert[2].position.x = 608;
+	vert[2].position.y = 448;
+	vert[2].color = { 0, 255, 0, 255 };
+
+	vert[3].position.x = 32;
+	vert[3].position.y = 448;
+	vert[3].color = { 0, 0, 255, 255 };
+
+	indices[0] = 0;
+	indices[1] = 1;
+	indices[2] = 3;
+	indices[3] = 1;
+	indices[4] = 2;
+	indices[5] = 3;
+
+	SDL_Event ev;
+	while (!quit)
+	{
+		while (SDL_PollEvent(&ev) != 0)
 		{
-			std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << "\n";
-		}
-		else
-		{
-			bool quit = false;
-
-			// Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
-
-			//Event handler
-			SDL_Event ev;
-			while (!quit)
+			if (ev.type == SDL_QUIT)
 			{
-				while (SDL_PollEvent(&ev) != 0)
-				{
-					//User requests quit
-					if (ev.type == SDL_QUIT)
-					{
-						quit = true;
-					}
-				}
-
-				// Fill the surface
-				SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0x22, 0x22, 0x55));
-
-				// Update the surface
-				SDL_UpdateWindowSurface(window);
+				quit = true;
+				break;
 			}
 		}
+
+		SDL_SetRenderDrawColor(renderer, 34, 34, 85, 255);
+		SDL_RenderClear(renderer);
+
+		// Draw stuff
+		SDL_RenderGeometry(renderer, nullptr, vert, vertSize, indices, 6);
+
+		SDL_RenderPresent(renderer);
 	}
 
-	// Destroy window
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
-
-	// Quit SDL subsystems
 	SDL_Quit();
 
 	return 0;
