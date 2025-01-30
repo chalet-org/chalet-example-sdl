306b0e768fb45ce7f0ce33c4c65cdef82937e2cf
Andrew King <pepsiotaku@gmail.com>
2025-01-29 21:47:05 -0500

    update linux platform for SDL3

--- src/Platform/Platform.cpp
+++ src/Platform/Platform.cpp
@@ -4,6 +4,8 @@
 
 #if defined(_WIN32)
 	#include "Platform/Win32/WindowsPlatform.hpp"
+#elif defined(__linux__)
+	#include <X11/Xlib.h>
 #endif
 
 namespace
@@ -28,34 +30,35 @@ WindowHandle getPlatformWindowHandle(SDL_Window* inWindow)
 {
 #if defined(_WIN32)
 	HWND hwnd = (HWND)SDL_GetPointerProperty(SDL_GetWindowProperties(inWindow), SDL_PROP_WINDOW_WIN32_HWND_POINTER, NULL);
-	return hwnd;
+	if (hwnd != nullptr)
+		return hwnd;
+
 #elif defined(__APPLE__)
 	auto handle = SDL_GetPointerProperty(SDL_GetWindowProperties(inWindow), SDL_PROP_WINDOW_COCOA_WINDOW_POINTER, NULL);
-	return handle;
+	if (handle != nullptr)
+		return handle;
+
 #elif defined(__linux__)
+	#if defined(SDL_VIDEO_DRIVER_WAYLAND)
+	if (SDL_strcmp(SDL_GetCurrentVideoDriver(), "wayland") == 0)
+	{
+		// struct wl_display* display = (struct wl_display*)SDL_GetPointerProperty(SDL_GetWindowProperties(inWindow), SDL_PROP_WINDOW_WAYLAND_DISPLAY_POINTER, NULL);
+		auto surface = (wl_surface*)SDL_GetPointerProperty(SDL_GetWindowProperties(inWindow), SDL_PROP_WINDOW_WAYLAND_SURFACE_POINTER, NULL);
+		return surface;
+	}
+	#endif
+	#if defined(SDL_VIDEO_DRIVER_X11)
 	if (SDL_strcmp(SDL_GetCurrentVideoDriver(), "x11") == 0)
 	{
 		// Display* xdisplay = (Display*)SDL_GetPointerProperty(SDL_GetWindowProperties(inWindow), SDL_PROP_WINDOW_X11_DISPLAY_POINTER, NULL);
 		auto xwindow = (Window)SDL_GetNumberProperty(SDL_GetWindowProperties(inWindow), SDL_PROP_WINDOW_X11_WINDOW_NUMBER, 0);
 		return xwindow;
 	}
-	else if (SDL_strcmp(SDL_GetCurrentVideoDriver(), "wayland") == 0)
-	{
-		// struct wl_display* display = (struct wl_display*)SDL_GetPointerProperty(SDL_GetWindowProperties(inWindow), SDL_PROP_WINDOW_WAYLAND_DISPLAY_POINTER, NULL);
-		auto surface = (wl_surface*)SDL_GetPointerProperty(SDL_GetWindowProperties(inWindow), SDL_PROP_WINDOW_WAYLAND_SURFACE_POINTER, NULL);
-		return surface;
-	}
-	else
-	{
-		std::cerr << "Couldn't get window information: " << SDL_GetError();
-		return 0;
-	}
-#else
-	{
-		std::cerr << "Couldn't get window information: " << SDL_GetError();
-		return 0;
-	}
+	#endif
 #endif
+
+	std::cerr << "Couldn't get window information: " << SDL_GetError();
+	return 0;
 }
 }
 
